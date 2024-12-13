e9817706b358deb00eaa8196da92158baceaed19
Shinyuy Marcel <shinyuy9@gmail.com>
2024-12-13 14:51:47 +0100

    Fix

--- app/activation/[uid]/[token]/page.tsx
+++ app/activation/[uid]/[token]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useActivationMutation } from '@/redux/features/authApiSlice';
+import { useActivationMutation } from '../../../../redux/features/authApiSlice';
 import { toast } from 'react-toastify';
 
 interface Props {
